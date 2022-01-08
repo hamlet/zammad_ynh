a6816a2c96236740d8aac8f4c6cf5de5ef751452
hamlet <h@mlet.me>
2022-01-08 13:55:16 +0100

    new try with packages

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="libimlib2 nodejs apt-transport-https curl git patch build-essential bison zlib1g-dev libssl-dev libxml2-dev libxml2-dev autotools-dev\
-  libxslt1-dev libyaml-0-2 autoconf automake libreadline-dev libyaml-dev libtool libgmp-dev libgdbm-dev libncurses5-dev\
-  pkg-config libffi-dev libimlib2-dev gawk libsqlite3-dev sqlite3"
+pkg_dependencies="libimlib2 nodejs apt-transport-https curl"
 
 #=================================================
 # PERSONAL HELPERS
